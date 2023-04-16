d83a5998ede6cd63fae8ebfa274b884426febf31
TADean <61359931+toDythetoad@users.noreply.github.com>
2023-04-16 17:19:32 -0400

    Changed player script name
    I changed the name of the script and class from PlayerMovment to PlayerBehavior

--- Assets/TD's work/PlayerBehavior.cs
+++ Assets/TD's work/PlayerBehavior.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerBehavior: MonoBehaviour
 {
     private string UserName = "Tim";
     [SerializeField] int score; //holds the current player score
